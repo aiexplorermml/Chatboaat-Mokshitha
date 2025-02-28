47a291a372dc70f28b2f4925f309b82ea76d3aff
aiexplorermml <156586900+aiexplorermml@users.noreply.github.com>
2025-02-28 10:07:08 +0530

    Update Mokshitha.py

--- Mokshitha.py
+++ Mokshitha.py
@@ -23,7 +23,7 @@ if prompt := st.chat_input("Say something..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
- response = st.session_state.chat.send_message(prompt)
+    response = st.session_state.chat.send_message(prompt)
 
     st.session_state.messages.append({"role": "assistant", "content": response.text})
     with st.chat_message("assistant"):
